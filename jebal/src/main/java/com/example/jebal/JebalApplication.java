6382826efb8c5aa60c14029d65b8ebf1bcd906b2
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-21 15:03:59 +0900

    ✅ :: file upload

---
+++ jebal/src/main/java/com/example/jebal/JebalApplication.java
@@ -0,0 +1,13 @@
+package com.example.jebal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JebalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JebalApplication.class, args);
+    }
+
+}
---
+++ jebal/src/test/java/com/example/jebal/JebalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jebal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JebalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
