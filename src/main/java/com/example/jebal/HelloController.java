71a4b6a18841e756c83717d6b56137516b15cb8d
kim seongwon <seongwon1290@dsm.hs.kr>
2022-10-22 15:36:41 +0900

    asdf

--- src/main/java/com/example/jebal/HelloController.java
+++ src/main/java/com/example/jebal/HelloController.java
@@ -1,38 +1,15 @@
 package com.example.jebal;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
 public class HelloController {
-    private final RedisTemplate<String, String> redisTemplate;
 
     @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
-
-    @PostMapping("/data")
-    public ResponseEntity<String> setRedisData(
-            @RequestBody(required = true) Map<String,String> map) throws Exception{
-
-        redisTemplate.opsForValue().set(map.get("key"), map.get("value"));
-
-        return new ResponseEntity<>("정상 등록", HttpStatus.CREATED);
-    }
-
-    @GetMapping("/data")
-    public ResponseEntity<String> getRedisData(
-            @RequestParam(required = true) String key){
-
-        return new ResponseEntity<>(redisTemplate.opsForValue().get(key), HttpStatus.OK);
-
-    }
-
 }
